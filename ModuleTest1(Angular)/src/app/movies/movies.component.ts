c88eea8a61a3888fd6f065279134acda2192119a
Kunal Kashyap <kunalkashyap159@gmail.com>
2019-12-20 20:12:35 +0530

    Angular(1st Module-Test)

---
+++ ModuleTest1(Angular)/src/app/movies/movies.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent implements OnInit {
+
+  search;
+  searchMovies;
+  constructor(public http: HttpClient) {   }
+
+  ngOnInit() {
+  }
+  // cFun(event) {
+  //   // console.log(event.target.value);
+  //   this.search = event.target.value;
+  //   console.log(this.search);
+  // }
+    // getSearchNews()
+     cFun(event) {
+      // this.search = event.target.value;
+    this.http.get<any>('http://www.omdbapi.com/?apikey=409135fb&s='+event.target.value).subscribe(data => {
+    this.searchMovies = data.Search;
+    }, err => {
+        console.log(err);
+    }, () => {
+        console.log('sourcesNews Got Successfuly');
+    });
+
+  }
+
+}
+
+
